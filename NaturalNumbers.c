b6a169b61f3c59b093dabd64f69924cce2d05414
laskariNagaraj <123875799+laskariNagaraj@users.noreply.github.com>
2023-02-05 18:00:09 +0530

    DAY 4 ASSIGNMENT SUBMITTED

---
+++ NaturalNumbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("Sum of 10 Natural Numbers\n");
+    int i = 0;
+    for(int i = 1 ; i<=10; i++){
+        
+        printf("%d\n", i);
+    }
+    
+    
+    return 0;
+}
